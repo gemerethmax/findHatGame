eb7f87a17104a3b677cbf5f8446be81e1a279774
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-06-11 10:05:58 -0400

    Constuctor and print method created for Field class.

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field = [[]]) {
+        this.field = field;
+        this.locationX = 0;
+        this.locationY = 0
+        this.field[0][0] = pathCharacter;
+    }
+
+    print() {
+        const displayString = this.field.map(row =>{
+             return row.join('')
+         }).join('\n');
+         console.log(displayString);
+    }
+}
